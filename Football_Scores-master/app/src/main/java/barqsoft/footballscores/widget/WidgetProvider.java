aef7cb3593e3acd778c05fead44aa1a6194e150b
Matej Vidakovic <matej.vidakovic@fiveminutes.eu>
2015-07-25 18:06:22 +0200

    widget size update and on click

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/WidgetProvider.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/WidgetProvider.java
@@ -1,8 +1,25 @@
 package barqsoft.footballscores.widget;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import barqsoft.footballscores.MainActivity;
+import barqsoft.footballscores.R;
 
 public class WidgetProvider extends AppWidgetProvider {
 
-
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for(int appWidgetId : appWidgetIds) {
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            remoteViews.setOnClickPendingIntent(R.id.widget_root, pendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+        }
+    }
 }
