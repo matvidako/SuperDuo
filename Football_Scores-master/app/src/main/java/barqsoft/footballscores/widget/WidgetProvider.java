6a93b50f3c3fb3c963e4eff8f34fdcf8fc608594
Matej Vidakovic <matej.vidakovic@fiveminutes.eu>
2015-10-24 19:08:43 +0200

    widget with update button

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/WidgetProvider.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/WidgetProvider.java
@@ -5,21 +5,47 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.util.Log;
+import android.view.View;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import barqsoft.footballscores.MainActivity;
 import barqsoft.footballscores.R;
+import barqsoft.footballscores.service.MatchFetchService;
 
 public class WidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        Toast.makeText(context, "Update!", Toast.LENGTH_SHORT).show();
         for(int appWidgetId : appWidgetIds) {
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
-            Intent intent = new Intent(context, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-            remoteViews.setOnClickPendingIntent(R.id.widget_root, pendingIntent);
+            setupOnRootClick(context, remoteViews);
+            setupOnUpdateClick(context, appWidgetIds, remoteViews);
             appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
         }
+
+        context.startService(new Intent(context, MatchFetchService.class));
     }
+
+    private void setupOnUpdateClick(Context context, int[] appWidgetIds, RemoteViews remoteViews) {
+        Intent intent = new Intent(context, WidgetProvider.class);
+        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
+    }
+
+    private void setupOnRootClick(Context context, RemoteViews remoteViews) {
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        remoteViews.setOnClickPendingIntent(R.id.widget_root, pendingIntent);
+    }
+
 }
